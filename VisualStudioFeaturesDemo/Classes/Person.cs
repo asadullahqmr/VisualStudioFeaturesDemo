236fe6af25c9450f2f853ef2df1d5144bc2927ef
Asadullah Qamar Bin Qamar Siddique Bhatti <asadullah@studentambassadors.com>
2020-12-06 19:37:04 +0800

    Add project files.

---
+++ VisualStudioFeaturesDemo/Classes/Person.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace VisualStudioFeaturesDemo.Classes
+{
+    public class Person
+    {
+        #region Properties
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FullName => FirstName + " " LastName;
+        public int Age { get; set; }
+        #endregion
+
+        #region Constructors
+        public Person()
+        {
+            FirstName = null;
+            LastName = null;
+            Age = 0;
+        }
+
+        public Person(string firstName, string lastName, int age)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+        }
+        #endregion
+
+        #region Functions
+        public static Person ReadPersonInput()
+        {
+            Console.WriteLine("First Name:");
+            var firstName = Console.ReadLine();
+            Console.WriteLine("Last Name:");
+            var lastName = Console.ReadLine();
+            Console.WriteLine("Age:");
+            var age = Int32.Parse(Console.ReadLine());
+
+            return new Person(firstName, lastName, age);
+        }
+
+        public static Person EvalAge(Person friend1, Person friend2)
+        {
+            return (friend1.Age > friend2.Age) ? friend1 : friend2;
+        }
+        #endregion
+
+        #region Overloads
+
+        public static bool operator ==(Person a, Person b)
+        {
+            return a.Age == b.Age;
+        }
+
+        public static bool operator !=(Person a, Person b)
+        {
+            return a.Age != b.Age;
+        }
+
+        #endregion
+    }
+}
---
+++ VisualStudioFeaturesDemo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using VisualStudioFeaturesDemo.Classes;
+
+namespace VisualStudioFeaturesDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter details of person 1.");
+            var friend1 = Person.ReadPersonInput();
+
+            Console.WriteLine(""); //New line
+            Console.WriteLine("Please enter details of person 2.");
+            var friend2 = Person.ReadPersonInput();
+
+            Console.WriteLine(""); //New line
+            Console.WriteLine(""); //New line
+
+            if (friend1 == friend2)
+            {
+                Console.WriteLine("Both people are the same age.");
+                return;
+            }
+
+            Person olderFriend = Person.EvalAge(friend1, friend2);
+            Console.WriteLine(string.Format("{0} is older.", olderFriend.FullName));
+        }
+    }
+}
